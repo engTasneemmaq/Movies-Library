d319d02b6d7e9512d2ed2563f884f28b05f8d341
engTasneemmaq <tasneemmaq@yahoo.com>
2022-05-16 10:25:36 +0300

    done

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
 'use strict';
 require('dotenv').config();
-const URL = "postgres://tasneem:12345@localhost:5432/movies"
-const port = process.env.PORT;
+const url = "postgres://tasneem:12345@localhost:5432/movies";
+const PORT = process.env.PORT;
 const dataJson = require("./data.json");
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios').default;
 const bodyParser = require('body-parser');
 const {
     Client
 } = require('pg');
 // const client = new Client(URL);
-const pg=require('pg');
+const pg = require('pg');
 const client = new Client({
     connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false }
- });
-const axios = require('axios').default;
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
 //const pg=require('pg');
 //const client=new pg.Client(URL);
 const apiKey = process.env.API_KEY;
@@ -36,140 +39,162 @@ app.put("/UPDATE/:id", handleUpdateMovie);
 app.delete("/DELETE/:id", handleDeleteMovie)
 app.use("*", handleNotFound);
 app.use(handleError);
+
 function handleHomePage(req, res) {
-    let newRecipe = new Recipe(dataJson.title, dataJson.poster_path, dataJson.overview, dataJson.comment);
-    res.json(newRecipe);
+    let newMovie = new Movie(dataJson.title, dataJson.poster_path, dataJson.overview, dataJson.comment);
+    res.json(newMovie);
 }
+
 function handleFavorite(req, res) {
     res.send("Welcome to Favorite Page");
 }
+
 function handleTrending(req, res) {
-    axios.get("https://api.themoviedb.org/3/trending/all/week?api_key=37ddc7081e348bf246a42f3be2b3dfd0&language=en-US")
+    let url = `https://api.themoviedb.org/3/trending/all/week?api_key=${apiKey}`;
+    axios.get(url)
         .then(result => {
             console.log(result.data.results);
-            let recipes = result.data.results.map(recipe => {
-                return new Recipe(recipe.id, recipe.title, recipe.release_date, recipe.poster_path, recipe.overview, recipe.comment);
-            })
-            res.json(recipes);
+            let Movies = result.data.results.map(movie => {
+                return new Movie(movie.id, movie.title, movie.release_date, movie.poster_path, movie.overview);
+            });
+            res.json(Movies);
         })
         .catch((error) => {
             console.log(error);
-            //res.send("Inside catch");
-        })
+            res.send("Inside catch")
+        });
 }
+
+
 function handleSearch(req, res) {
-    let recipeName = req.query.recipeName;
-    let url = `https://api.themoviedb.org/3/search/movie?query=${recipeName}&api_key=668baa4bb128a32b82fe0c15b21dd699`;
+    let movieName = req.query.movieName;
+    let url = `https://api.themoviedb.org/3/search/movie?query=${movieName}&api_key=${apiKey}`;
     axios.get(url)
         .then(result => {
             res.json(result.data.results);
         })
         .catch((error) => {
-            console.log(error);
-            //res.send("Inside catch");
+            console.log(error.message);
+
+
         })
-    let recipeName2 = req.query.recipeName2;
-    let url2 = `https://api.themoviedb.org/3/search/keyword?query=${recipeName2}&api_key=668baa4bb128a32b82fe0c15b21dd699`;
-    axios.get(url2)
-        .then(result => {
-            res.json(result.data.results);
-        })
-        .catch((error) => {
-            console.log(error);
-            //res.send("Inside catch");
-        })
-    let recipeName3 = req.query.recipeName3;
-    let url3 = `https://api.themoviedb.org/3/search/person?query=${recipeName3}&api_key=668baa4bb128a32b82fe0c15b21dd699`;
-    axios.get(url3)
-        .then(result => {
-            res.json(result.data.results);
-        })
-        .catch((error) => {
-            console.log(error);
-            //res.send("Inside catch");
-        })
+
 }
+
+
 function handleNotFound(req, res) {
     res.send("Page not found");
 }
+
 function handleError(err, req, res) {
     res.status(500).send(err);
 }
-function handleAddMovies(req, res) {
-    const {
-        title,
-        release_date,
-        poster_path,
-        overview,
-        comment
-    } = req.body;
-    let sql = `INSERT INTO movie(title,release_date,poster_path,overview,comment) VALUES($1, $2, $3, $4, $5) ;`
-    let values = [ title, release_date, poster_path, overview, comment];
-    client.query(sql, values).then((result) => {
+
+
+function handleAddMovies(req,res){
+    const{title,release_date,poster_path,overview}=req.body;
+
+    let sql=`INSERT INTO movie(title,release_date,poster_path,overview) VALUES($1, $2, $3, $4) RETURNING *;`
+    let values=[title,release_date,poster_path,overview];
+    client.query(sql,values).then((result)=>{
         console.log(result.rows);
         return res.status(201).json(result.rows[0]);
     }).catch((err) => {
         handleError(err, req, res);
     });
-}
+    }
+
 function handleGetMovies(req, res) {
+
     let sql = 'SELECT * from movie;'
     client.query(sql).then((result) => {
+        console.log(result);
         res.json(result.rows);
     }).catch((err) => {
         handleError(err, req, res);
-    })
+    });
 }
-function handleGetMovieByID(req, res) {
-    const id = req.params.id;
-    console.log(id);
-    const sql = `SELECT * FROM movie WHERE id=${id};`
-    client.query(sql).then(result => {
-        console.log(result);
-        res.status(200).json(result.rows);
-    }).catch(error => {
-        console.log(error);
-        handleError(error, req, res);
-    })
+
+function handleError(error, req, res) {
+    res.status(500).send(error)
 }
+
 function handleUpdateMovie(req, res) {
-    const id = req.params.id;
-    const upMov = req.body;
-    const sql = `UPDATE movie SET title=$1, release_date=$2, poster_path=$3, overview=$4,comment=$5 WHERE id=${id} RETURNING *;`;
-    const values = [upMov.title, upMov.release_date, upMov.poster_path, upMov.overview,upMov.comment];
+    const id = req.params;
+    const {
+        title,
+        release_date,
+        poster_path,
+        overview
+    } = req.body;
+
+    let sql = `UPDATE movie SET  title = $1, release_date= $2, poster_path = $3,overview=$4 WHERE id =$5 RETURNING *;`
+    let values = [title, release_date, poster_path, overview];
     client.query(sql, values).then(data => {
         return res.status(200).json(data.rows);
     }).catch(error => {
         handleError(error, req, res);
     })
 }
+
+
 function handleDeleteMovie(req, res) {
-   /* const {
-        movieId
-    } = req.params;*/
-    const id = req.params.id;
-        let sql = `DELETE from movie WHERE id = ${id};`
-   // let value = [movieId];
-    client.query(sql).then((result =>
-        res.send("movie deleted")
-    )).catch((err =>
-        handleError(err, req, res)
-    ))
+    const newID = req.params.id;
+    let value = [newID];
+
+    const sql = `DELETE FROM movie WHERE id = $5;`
+    client.query(sql, value)
+        .then(() => {
+            return res.status(204).json([]);
+        })
+        .catch(error => {
+            handleError(error, req, res);
+        })
+
 }
+
+function handleGetMovieByID(req, res) {
+    const {
+        id
+    } = req.query;
+
+    const sql = 'SELECT * from movie WHERE id =$1 ;'
+    let value = [id];
+    client.query(sql, value).then(result => {
+        res.status(200).json(result.rows);
+
+    }).catch(error => {
+        console.log(error);
+        handleError(error, req, res);
+    })
+}
+
+
+
 client.connect().then(() => {
-    app.listen(port, () => {
-        console.log(`Server is listening ${port}`);
+    app.listen(PORT, () => {
+        console.log(`Server is listening ${PORT}`);
     });
 })
-/*app.listen(port, handleListen);
-function handleListen() {
-    console.log(`Example app listening on port ${port}`);
-}*/
-function Recipe(id, title, release_date, poster_path, overview, comment) {
+
+
+
+
+
+
+// constrructor for handelhomepage
+function Slecet(title, poster_path, overview) {
+    this.title = title,
+        this.poster_path = poster_path,
+        this.overview = overview
+}
+
+
+// constructor for trending
+function Movie(id, title, release_date, poster_path, overview) {
     this.id = id;
     this.title = title;
     this.release_date = release_date;
     this.poster_path = poster_path;
-    this.overview = overview;
-    this.comment = comment;
+    this.overview = overview
 }
